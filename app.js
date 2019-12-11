ee36add9332dd441e786332fde3bb16af380c1f4
Beselam-Asefa <gbeselam@gmail.com>
2019-12-11 13:15:26 +0200

    final

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 4000;
+const port = 40000;
 const passport = require('passport');
 
 require('./passport')(passport);
